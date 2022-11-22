bb0ae6616cfa3c9755ba3a1078d14778f34c226f
Marc <duguay.marc@gmail.com>
2022-11-22 16:52:48 -0500

    New Gui

---
+++ gui.py
@@ -0,0 +1,15 @@
+from Modules import functions
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a todo")
+input_box = sg.InputText(tooltip = "Enter todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My Todo App', layout=[[label],[input_box,add_button]])
+window.read()
+
+window.close()
+
+
+
+
