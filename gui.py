1c428404fd22ddcb814aa44001352c70ac244cb4
Marc <duguay.marc@gmail.com>
2022-11-23 17:48:09 -0500

    Added the Add functionality and window close

--- gui.py
+++ gui.py
@@ -2,11 +2,29 @@ from Modules import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a todo")
-input_box = sg.InputText(tooltip = "Enter todo")
+input_box = sg.InputText(tooltip= "Enter todo",key = "todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My Todo App', layout=[[label],[input_box,add_button]])
-window.read()
+window = sg.Window('My Todo App',
+                   layout=[[label],
+                   [input_box, add_button]],
+                   font=('Helvetica', 12))
+while 1:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
+
+
 
 window.close()
 
