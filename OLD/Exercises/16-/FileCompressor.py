7852a216338884237c5fbb04ad6c1764ee84db2a
Marc <duguay.marc@gmail.com>
2022-11-28 15:50:55 -0500

    Implement Exit and Complete Buttons

--- OLD/Exercises/16-/FileCompressor.py
+++ OLD/Exercises/16-/FileCompressor.py
@@ -1,17 +1,36 @@
 import PySimpleGUI as sg
+from ZipCreator import make_archive
 
 label1 = sg.Text("Select files to compress")
 input1 = sg.Input(tooltip= "Select directory")
-choose_button1 = sg.FilesBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose",key="files")
 
 label2 = sg.Text("Select destination folder")
 input2 = sg.Input(tooltip= "Select directory")
-choose_button2 = sg.FolderBrowse("Choose")
+choose_button2 = sg.FolderBrowse("Choose",key="folder")
 
 compress_button = sg.Button("Compress")
+outputlabel = sg.Text(key="output", text_color='green')
 
 window = sg.Window('File Compressor', layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
-                                              [compress_button]])
-window.read()
+                                              [compress_button,outputlabel]])
+
+while 1:
+    event,values = window.read()
+    print(event)
+    print(values)
+    filepaths = values["files"].split(";")
+    folder = values["folder"]
+    make_archive(filepaths,folder)
+    window["output"].update(value="Compression completed")
+    for value in values:
+        print(value,' : ',values[value])
+    match event:
+        #case "Compress":
+
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
--- gui.py
+++ gui.py
@@ -4,25 +4,54 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip= "Enter todo",key = "todo")
 add_button = sg.Button("Add")
+todos = functions.get_todos()
+list_box = sg.Listbox(values=todos, key='todos',
+                      enable_events=True,size=[45,10])
+edit_button = sg.Button("Edit")
+exit_button = sg.Button("Exit")
+complete_button = sg.Button("Complete")
 
 window = sg.Window('My Todo App',
-                   layout=[[label],
-                   [input_box, add_button]],
+                   layout=[
+                   [label],
+                   [input_box, add_button],
+                   [list_box, edit_button, complete_button],
+                   [exit_button]
+                   ],
                    font=('Helvetica', 12))
 while 1:
     event, values = window.read()
-    print(event)
-    print(values)
+    print("Event: ", event)
+    print("Values: ", values)
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
+        case "todos":
+            window['todo'].update(value = values['todos'][0])
         case sg.WIN_CLOSED:
             break
-
-
 
 
 
