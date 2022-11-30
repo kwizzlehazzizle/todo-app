cb8d8ccefed5ce3534e296db88dda2c49941b70a
Marc <duguay.marc@gmail.com>
2022-11-30 09:44:38 -0500

    Add add field

--- web.py
+++ web.py
@@ -3,9 +3,20 @@ from Modules import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo+'\n')
+    functions.write_todos(todos)
+
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo:", placeholder="Add a new todo: ")
+st.text_input(label="Enter a todo:", placeholder="Add a new todo: ",
+              on_change=add_todo, key='new_todo')
+
+
+st.session_state
